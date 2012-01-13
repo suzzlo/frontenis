6546635972bb01de6ef5611c6efed57045b547f1
Ion Gonzalez <ion@suzzlo.org>
2012-01-13 19:22:48 +0100

    Initial inport

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontenis::Application.config.secret_token = '2ec442a545f4bc90e85a0e1b2a35e71755319ea09211f4eaa727cd9e5e76acd0122bdab2caf2565d4e75aa76e02f37b689f1284a585c9561ab673e39fccedf0c'
